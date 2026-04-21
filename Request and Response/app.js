000ecbb6c560ec8fb10bc281b60fa454038518de
Priyal Shrivastava <priyalshrivastava006@gmail.com>
2026-04-21 21:55:17 +0530

    commit

--- Request and Response/app.js
+++ Request and Response/app.js
@@ -1,10 +1,16 @@
 const http = require('http');
 
 const server = http.createServer( function(req, res){
-  console.log(req);
+  console.log(req.url, request.method, request.headers);
+  res.setHeaders('Content-Type', 'text/html');
+  res.write('<html>');
+  res.write('<head><title>Complete coding</title></head>');
+  res.write('<body><h1>Like and share</h1></body>');
+  res.write('</html>');
+  res.end();
 });
 
 const PORT = 3000;
 server.listen(PORT, () => {
-  console.log('Server running on address http://localhost:${PORT} ')
+  console.log('Server running on address http://localhost:${PORT}')
 });
