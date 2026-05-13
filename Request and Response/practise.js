47f17ac4cc8007a332ca2eabfc0fbe8062851f67
Priyal Shrivastava <priyalshrivastava006@gmail.com>
2026-05-13 19:16:58 +0530

    node js

---
+++ Request and Response/practise.js
@@ -0,0 +1,11 @@
+const http = require('http');
+
+const server = http.createServer( function(req, res){
+  console.log(req.url, req.method);
+  res.write('<>');
+});
+
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log('Server running on address http://localhost:${PORT}')
+});
